c0b30c758dc5715a68f122ac17deb73c58d7ae6f
Joey J <j2500jj@gmail.com>
2025-04-14 09:47:12 -0500

    guh

--- test.py
+++ test.py
@@ -1,22 +1,50 @@
-import turtle
+from turtle import *
 
-def jump():
-    player.forward(20)
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.speed(0)
+        self.setpos(-200,0)
+        self.shape('square')
+        self.left(90)
+        self.showturtle()
+        screen.onkey(self.jump, 'Up')
+    
+    def jump(self):
+        self.forward(70)
 
-screen = turtle.Screen()
+    def gravity(self):
+        self.sety(self.ycor() - 5)
+
+class Pipe(Turtle):
+    def __init__(self, y, x):
+        super().__init__()
+        self.hideturtle()
+        self.speed(0)
+        self.penup()
+        self.color('green')
+        self.shape('square')
+        self.shapesize(15, 2)
+        self.setpos(x, y)
+        self.setheading(180)
+        self.showturtle()
+
+    def move(self):
+        self.forward(5)
+
+screen = Screen()
 screen.title('Game')
+screen.listen()
 
-player = turtle.Turtle()
-player.shape('square')
-player.penup()
-player.left(90)
+bird = Player()
+upper = Pipe(250, 300)
+lower = Pipe(-250, 300)
 
-x_num = player.xcor()
-y_num = player.ycor()
-
-print(f'x = {x_num} \ny = {y_num}')
-
-screen.onkeypress(jump, 'Up')
-screen.listen() 
+while True:
+    bird.gravity()
+    upper.move()
+    lower.move()
 
 screen.mainloop()
